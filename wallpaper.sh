b3bf45306d13a9adfa54ba1414f54a1552b5cbc5
CallumAustinTaylor <callumaustintaylor@proton.me>
2025-09-07 20:39:13 +0100

    Fixed path detection in wallpaper.sh.

--- wallpaper.sh
+++ wallpaper.sh
@@ -1,6 +1,12 @@
 #!/run/current-system/sw/bin/bash
 
-wallpaper="$(pwd)/$1"
+
+# Handle both absolute and relative paths
+if [[ "$1" = /* ]]; then
+    wallpaper="$1"  # Already absolute path
+else
+    wallpaper="$(pwd)/$1"  # Make relative path absolute
+fi
 
 swww img $wallpaper
 
